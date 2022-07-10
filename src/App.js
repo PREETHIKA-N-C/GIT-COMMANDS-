3c8d58f859d40cbee4be8fabab4b5085828ad31a
Preethika <ncpreethika30@gmail.com>
2022-07-10 13:02:41 +0530

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Footer from "./Footer"; 
 function App() {
   return (
     <div className="App">
-     
+     <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+        <p>© 2022 PREETHIKA</p>
+    </footer>
+    
+  );
+}
+
+export default Footer;
