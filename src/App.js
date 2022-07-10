5bfd9adb4e2965092ebb1be3b743fab910caae65
Preethika <ncpreethika30@gmail.com>
2022-07-10 12:55:33 +0530

    Add Genres

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Genres from "./Genres";
 
 function App() {
   return (
     <div className="App">
-     
+     <Genres></Genres>
     </div>
   );
 }
---
+++ src/Genres.js
@@ -0,0 +1,21 @@
+function Genres() {
+  return (
+    <div className="App">
+      <h2>BOOK GENRES</h2>
+      <ul>
+        <li>HORROR</li>
+        <li>CRIME</li>
+        <li>SCIENCE FICTION</li>
+        <li>MYSTERY</li>
+        <li>THRILLER</li>
+        <li>HISTORICAL FICTION</li>
+        <li>ADVENTURE FICTION</li>
+        <li>BIOGRAPHY</li>
+        <li>SELF-HELP</li>
+        <li>FANTASY</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Genres;
