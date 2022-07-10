fb105a9e12bcde5c01a923c96bef51d365264b9a
Preethika <ncpreethika30@gmail.com>
2022-07-10 12:59:07 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Genres from "./Genres";
-
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
      <Genres></Genres>
+     <Header></Header>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="App">
+      <h1>E-LIBRARY</h1>
+    </div>
+  );
+}
+
+export default Header;
